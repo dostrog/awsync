5d194652ff20d635dee3454d6201bc2a218101ab
Sergey Donin <sergey.donin@gmail.com>
2021-02-28 19:29:16 +0100

    Add some translation

--- resources/lang/sl/awsync.php
+++ resources/lang/sl/awsync.php
@@ -5,10 +5,16 @@ return [
     'nonzero' => 'Število datotek mora biti večje od 0',
     'errors' => [
         'populate' => 'Datoteke ni mogoče zapolniti: :message',
+        'no_file_from_aws' => 'Datoteke ni mogoče prebrati iz AWS S3',
+        'no_write_to_aws' => 'Datoteke ni mogoče zapisati v AWS S3',
     ],
     'info' => [
         'populate_common' => 'Poselitev splošnih datotek...',
         'populate_named' => 'Poselitev datoteke z istim imenon...',
-        'done' => 'done.',
+        'done' => 'končano.',
+        'current_status' => 'Trenutno stanje mape / segmenta:: ime',
+        'status' => 'Stanje',
+        'quantity' => 'Količina',
+        'total' => 'Skupaj, Kb',
     ],
 ];
